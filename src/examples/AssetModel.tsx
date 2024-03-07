a295d21564f324b08a424f36e7c5b7966481ccdd
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2024-03-07 19:56:22 +0100

    Add Suzanne example

---
+++ src/examples/AssetModel.tsx
@@ -0,0 +1,27 @@
+import { OrbitControls, Stats, useGLTF } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
+
+export const AssetModel = () => {
+  const gltf = useGLTF('./suzanne/Suzanne.gltf');
+
+  return (
+    <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
+      <directionalLight
+        position={[3.3, 1.0, 4.4]}
+        castShadow
+        intensity={Math.PI * 2}
+      />
+      <primitive
+        object={gltf.scene}
+        position={[0, 1, 0]}
+        children-0-castShadow
+      />
+      {/* <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
+        <meshStandardMaterial />
+      </Circle> */}
+      <OrbitControls target={[0, 1, 0]} />
+      <axesHelper args={[5]} />
+      <Stats />
+    </Canvas>
+  )
+}
